33192af416ab6e4dce58f427f4d83b05c3812833
matveychuk <matveychuk.a@gmail.com>
2023-12-09 14:04:50 +0100

    update api url

--- client/src/config.js
+++ client/src/config.js
@@ -1,6 +1,6 @@
 const config = {
   // api: "http://localhost:4000",
-  api: "https://fierce-brook-46941.herokuapp.com",
+  api: "https://gfserver2024-28760ddf3c46.herokuapp.com",
 };
 
 export default config;
